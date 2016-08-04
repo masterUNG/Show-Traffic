25f317c65e75e7ca11eedb72d602e34e158427c7
masterUNG <phrombutr@gmail.com>
2016-08-04 14:57:36 +0700

    Sound Effect

--- app/src/main/java/appewtc/masterung/showtraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/showtraffic/MainActivity.java
@@ -1,13 +1,40 @@
 package appewtc.masterung.showtraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView listView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        listView = (ListView) findViewById(R.id.listView);
+        button = (Button) findViewById(R.id.button);
+
+        //Button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.cow);
+                mediaPlayer.start();
+
+            }   // onClick
+        });
+
+    }   // Main Method
+
+
+}   // Main Class นี่คือคลาสหลัก
