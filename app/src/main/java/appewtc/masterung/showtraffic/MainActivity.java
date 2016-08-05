946f983534af53788a5ad5c173194215f5d46ffc
masterUNG <phrombutr@gmail.com>
2016-08-05 11:55:29 +0700

    Create ListView Success

--- app/src/main/java/appewtc/masterung/showtraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/showtraffic/MainActivity.java
@@ -3,8 +3,8 @@ package appewtc.masterung.showtraffic;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -40,6 +40,46 @@ public class MainActivity extends AppCompatActivity {
 
             }   // onClick
         });
+
+        //ListView Controller
+        //Setup Array Type Easy
+        int[] iconInts = new int[20];
+        iconInts[0] = R.drawable.traffic_01;
+        iconInts[1] = R.drawable.traffic_02;
+        iconInts[2] = R.drawable.traffic_03;
+        iconInts[3] = R.drawable.traffic_04;
+        iconInts[4] = R.drawable.traffic_05;
+        iconInts[5] = R.drawable.traffic_06;
+        iconInts[6] = R.drawable.traffic_07;
+        iconInts[7] = R.drawable.traffic_08;
+        iconInts[8] = R.drawable.traffic_09;
+        iconInts[9] = R.drawable.traffic_10;
+        iconInts[10] = R.drawable.traffic_11;
+        iconInts[11] = R.drawable.traffic_12;
+        iconInts[12] = R.drawable.traffic_13;
+        iconInts[13] = R.drawable.traffic_14;
+        iconInts[14] = R.drawable.traffic_15;
+        iconInts[15] = R.drawable.traffic_16;
+        iconInts[16] = R.drawable.traffic_17;
+        iconInts[17] = R.drawable.traffic_18;
+        iconInts[18] = R.drawable.traffic_19;
+        iconInts[19] = R.drawable.traffic_20;
+
+        //Setup Array from xml
+        String[] titleStrings = getResources().getStringArray(R.array.title);
+        String[] detailLongStrings = getResources().getStringArray(R.array.detail);
+
+        //การตัดคำ
+        String[] detailShortStrings = new String[detailLongStrings.length];
+        for (int i=0;i<detailLongStrings.length;i+=1) {
+
+            detailShortStrings[i] = detailLongStrings[i].substring(0, 30) + "...";
+
+        }   //for
+
+        //Create ListView
+        TrafficAdapter trafficAdapter = new TrafficAdapter(this, iconInts, titleStrings, detailShortStrings);
+        listView.setAdapter(trafficAdapter);
 
     }   // Main Method
 
